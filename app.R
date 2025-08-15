50bf63109a0ebdf8b4e8649f3b5f471a69abff7a
Eric Scott <scottericr@gmail.com>
2025-08-15 14:19:17 -0700

    put multiple plots in a swiper

--- app.R
+++ app.R
@@ -10,8 +10,8 @@ thematic_shiny(font = "auto")
 #global options
 options <- f7DefaultOptions()
 options$color <- "#0C234B" #arizona blue
-options$dark <- TRUE
-options$theme <- "ios"
+options$dark <- FALSE
+options$theme <- "auto"
 
 # pre-load station data
 stations <- station_info$meta_station_id
@@ -33,17 +33,27 @@ ui <- f7Page(
         label = "Select a Station",
         choices = stations
       ),
-      f7Select(
-        inputId = "var",
-        label = "Choose a Variable",
-        choices = c("precip", "temp")
-      ),
-      outline = TRUE,
-      dividers = TRUE,
+      # f7DatePicker(
+      #   inputId = "daterange",
+      #   label = "Date Range",
+      #   rangePicker = TRUE
+      # ),
+      # outline = TRUE,
+      # dividers = TRUE,
       strong = TRUE
     ),
-    f7Card(
-      plotOutput("temp")
+    f7Swiper(
+      id = "plots_swiper",
+      f7Slide(
+        f7Card(
+          plotOutput("temp")
+        )
+      ),
+      f7Slide(
+        f7Card(
+          plotOutput("precip")
+        )
+      )
     )
   ),
   options = options,
@@ -58,7 +68,7 @@ server <- function(input, output, session) {
         lubridate::hours(24)
     )
   }) |>
-    shiny::bindEvent(input$btn_refresh)
+    shiny::bindEvent(input$btn_refresh, ignoreNULL = FALSE)
 
   data_station <- reactive({
     req(input$station)
@@ -69,7 +79,14 @@ server <- function(input, output, session) {
   output$temp <- renderPlot({
     data_station() |>
       ggplot(aes(x = datetime, y = temp_airC)) +
-      geom_line()
+      geom_line() +
+      labs(title = "Air Temperature (ºC)", x = NULL)
+  })
+  output$precip <- renderPlot({
+    data_station() |>
+      ggplot(aes(x = datetime, y = precip_total_mm)) +
+      geom_col() +
+      labs(title = "Precipitation (mm)", x = NULL)
   })
 }
 
